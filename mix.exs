20f1ea3854d2297ae8af60f7d28937d7442de833
Michael Truog <mjtruog@gmail.com>
2014-10-30 18:45:21 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule CloudIServiceHttpCowboy do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_http_cowboy,
+     version: "1.4.0-rc.4",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:cowboy,
+      [git: "https://github.com/ninenines/cowboy.git",
+       # 1.0.0 with fixes
+       ref: "b57f94661f5fd186f55eb0fead49849e0b1399d1"]},
+     {:cloudi_core, "~> 1.4.0-rc.4"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework cowboy HTTP/HTTPS Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_http_cowboy"}]
+   end
+end
