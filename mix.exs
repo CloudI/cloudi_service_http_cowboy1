95eb4d48106d24954690c1972cb086aac49aefb2
Michael Truog <mjtruog@gmail.com>
2014-12-20 18:05:21 -0800

    Version 1.4.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceHttpCowboy do
 
   def project do
     [app: :cloudi_service_http_cowboy,
-     version: "1.4.0-rc.4",
+     version: "1.4.0",
      language: :erlang,
      description: description,
      package: package,
@@ -15,7 +15,7 @@ defmodule CloudIServiceHttpCowboy do
       [git: "https://github.com/ninenines/cowboy.git",
        # 1.0.0 with fixes
        ref: "b57f94661f5fd186f55eb0fead49849e0b1399d1"]},
-     {:cloudi_core, "~> 1.4.0-rc.4"}]
+     {:cloudi_core, "~> 1.4.0"}]
   end
 
   defp description do
