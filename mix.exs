d9e49199789450fee1ead506e366b57451afc276
Michael Truog <mjtruog@protonmail.com>
2018-03-29 17:41:01 -0700

    Switch cloudi.org links from http to https.

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule CloudIServiceHttpCowboy do
     [files: ~w(src doc rebar.config README.markdown),
      maintainers: ["Michael Truog"],
      licenses: ["MIT"],
-     links: %{"Website" => "http://cloudi.org",
+     links: %{"Website" => "https://cloudi.org",
               "GitHub" => "https://github.com/CloudI/" <>
                           "cloudi_service_http_cowboy"}]
    end
