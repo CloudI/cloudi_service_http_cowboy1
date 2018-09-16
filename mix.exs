e2fd95e0bdf02c35350d3f7d4be8e80326df8367
Michael Truog <mjtruog@protonmail.com>
2018-09-15 20:51:38 -0700

    Version 1.7.4.

--- mix.exs
+++ mix.exs
@@ -6,8 +6,23 @@ defmodule CloudIServiceHttpCowboy do
 
   def project do
     [app: :cloudi_service_http_cowboy,
-     version: "1.7.3",
+     version: "1.7.4",
      language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
      description: description(),
      package: package(),
      deps: deps()]
@@ -15,7 +30,7 @@ defmodule CloudIServiceHttpCowboy do
 
   defp deps do
     [{:cowboy, "~> 1.0.1"},
-     {:cloudi_core, "~> 1.7.3"}]
+     {:cloudi_core, "~> 1.7.4"}]
   end
 
   defp description do
